dcd20efcf0a963e9ab7a55e7e6ca109e27c54baa
kamo2310 <90981867+kamo2310@users.noreply.github.com>
2021-09-27 21:12:50 -0600

    Add files via upload

---
+++ ClothingStore.BL/ClothingStore.Web/Controllers/ProductosController.cs
@@ -0,0 +1,22 @@
+﻿using ClothingStore.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var productosBL = new ProductosBL();
+            var listadeproductos = productosBL.ObtenerProductos();
+
+            return View(listadeproductos);
+        }
+
+    }
+}
